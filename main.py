5903fd772f32628ebade329ef1193299c80ee91d
Alex <78610101+Ultimatevirus@users.noreply.github.com>
2026-08-29 11:59:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import sqlite3
+import flask
+import json
+
+def start_exam:
+return(level, question_amount)
+
+def get_question(question_amount):
+return(question, answers)
+
+def exam(questions, answers)
+return(amount_correct, amount_false)
+
+def try_again_loop():
+
+
+if __name__ == __main__:
